1c8fd2c5393a24cc7b60b9b543b6ba00ca8fc4f3
katochsenthal <katoch.senthal@gmail.com>
2022-04-01 19:00:39 -0400

    displaying current weather on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,33 @@ var getWeather = function (city) {
       return response.json();
     })
     .then(function (data) {
-      renderWeatherData(data);
+      console.log(data);
+
+      var currentDate = moment().format("MMMM Do YYYY");
+      var weatherNow = data.main.temp;
+      var windSpeedNow = data.wind.speed;
+      var humidityNow = data.main.humidity;
+
+      var cityHeading = document.createElement("h2");
+      cityHeading.innerHTML = city + ", " + currentDate;
+      currentWeatherEl.append(cityHeading);
+      console.log("city Heading:", cityHeading);
+
+      var currentWeatherTemp = document.createElement("p");
+      currentWeatherTemp.innerHTML = "Temp: " + weatherNow + " ℃";
+      currentWeatherEl.append(currentWeatherTemp);
+
+      var currentWeatherWind = document.createElement("p");
+      currentWeatherWind.innerHTML = "Wind : " + windSpeedNow + " m/s";
+      currentWeatherEl.append(currentWeatherWind);
+
+      var currentWeatherHumidity = document.createElement("p");
+      currentWeatherHumidity.innerHTML = "Humidity : " + humidityNow + " %";
+      currentWeatherEl.append(currentWeatherHumidity);
     });
 };
+
+var fiveDayWeather = function () {};
 
 // var renderWeatherData = function (data) {
 //   console.log(data);
