9f2d2610796efaea2cc0aa7ff7f04289632b0c80
katochsenthal <katoch.senthal@gmail.com>
2022-04-01 20:40:52 -0400

    added box and displaying uv index

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,14 @@ var searchCityEl = document.querySelector(".searchCity");
 var searchBtnEl = document.querySelector(".searchBtn");
 var currentWeatherEl = document.querySelector(".currentWeather");
 
-var getWeather = function (city) {
-  var apiKey = "607439a3ad59adef49501bad43c27015";
+var apiKey = "607439a3ad59adef49501bad43c27015";
 
+var city = function () {
+  var cityName = searchCityEl.value;
+  getWeather(cityName);
+};
+
+var getWeather = function (city) {
   fetch(
     "https://api.openweathermap.org/data/2.5/weather?q=" +
       city +
@@ -16,17 +21,16 @@ var getWeather = function (city) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-
       var currentDate = moment().format("MMMM Do YYYY");
       var weatherNow = data.main.temp;
       var windSpeedNow = data.wind.speed;
       var humidityNow = data.main.humidity;
+      var cityLon = data.coord.lon;
+      var cityLat = data.coord.lat;
 
       var cityHeading = document.createElement("h2");
       cityHeading.innerHTML = city + ", " + currentDate;
       currentWeatherEl.append(cityHeading);
-      console.log("city Heading:", cityHeading);
 
       var currentWeatherTemp = document.createElement("p");
       currentWeatherTemp.innerHTML = "Temp: " + weatherNow + " ℃";
@@ -39,23 +43,33 @@ var getWeather = function (city) {
       var currentWeatherHumidity = document.createElement("p");
       currentWeatherHumidity.innerHTML = "Humidity : " + humidityNow + " %";
       currentWeatherEl.append(currentWeatherHumidity);
+
+      getUv(cityLon, cityLat);
+    });
+};
+
+var getUv = function (lon, lat) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lon +
+      "&lon=" +
+      lat +
+      "&exclude=hourly,daily&appid=" +
+      apiKey
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var weatherUv = data.current.uvi;
+      var uvEl = document.createElement("p");
+      uvEl.innerHTML = "UV Index: " + weatherUv;
+      currentWeatherEl.append(uvEl);
     });
 };
 
 var fiveDayWeather = function () {};
 
-// var renderWeatherData = function (data) {
-//   console.log(data);
-//   var cityTemp = data.main.temp;
-//   var cityTempDescription = data.weather[0].description;
-//   // creating elements
-//   // append to dom
-//   // remove existing elements
-//   var document.createElement("")
-//   currentWeather.appendChild()
-
-// };
 searchBtnEl.addEventListener("click", function () {
-  var cityName = searchCityEl.value;
-  getWeather(cityName);
+  city();
 });
